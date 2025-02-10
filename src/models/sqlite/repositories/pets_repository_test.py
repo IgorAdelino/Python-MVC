c94801eb0bce7e7200487fd817ba85351dcbdf08
Igor Adelino <igoradelino@gmail.com>
2025-02-10 14:28:28 -0300

    feat: no result test

--- src/models/sqlite/repositories/pets_repository_test.py
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -2,6 +2,8 @@ from unittest import mock
 from .pets_repository import PetsRepository
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 from src.models.sqlite.entities.pets import PetsTable
+from sqlalchemy.orm.exc import NoResultFound
+
 import pytest
 
 
@@ -18,6 +20,18 @@ class MockConnection:
         )
       ]
     )
+  def __enter__(self):
+      return self
+  def __exit__(self, exc_type, exc_value, traceback):
+      pass
+  
+class MockConnectionNoResult:
+  def __init__(self) -> None:
+    self.session = UnifiedAlchemyMagicMock()
+    self.session.query.side_effect = self.__raise_no_result_found
+
+  def __raise_no_result_found(self, *args, **kwargs):
+     raise NoResultFound("No result found")
   def __enter__(self):
       return self
   def __exit__(self, exc_type, exc_value, traceback):
@@ -44,4 +58,29 @@ def test_delete_pet():
    mock_connection.session.filter.assert_called_once_with(PetsTable.id == 1)
    mock_connection.session.delete.assert_called_once()
 
-  
+def test_list_pets_no_results():
+  mock_connection = MockConnectionNoResult()
+  repo = PetsRepository(mock_connection)
+  response = repo.list_pets()
+
+  mock_connection.session.query.assert_called_once_with(PetsTable)
+  mock_connection.session.all.assert_not_called()
+
+  assert response == []
+
+def test_delete_pet():
+   mock_connection = MockConnection()
+   repo = PetsRepository(mock_connection)
+
+   repo.delete_pet(1)
+
+   mock_connection.session.query.assert_called_once_with(PetsTable)
+   mock_connection.session.filter.assert_called_once_with(PetsTable.id == 1)
+   mock_connection.session.delete.assert_called_once()
+
+def test_delete_pet_error():
+  mock_connection = MockConnectionNoResult()
+  repo = PetsRepository(mock_connection)
+  with pytest.raises(Exception):
+    repo.delete_pet(1)
+  mock_connection.session.query.assert_called_once_with(PetsTable)
